0383d43f3900fdb3e05dd831c22b83caa238c5f8
Gareth Warburton <garethw738@gmail.com>
2025-07-03 13:09:50 +0200

    [0.1.4] Bugfix: Removed attributes from class docstring

--- src/nosible/classes/search_set.py
+++ src/nosible/classes/search_set.py
@@ -12,25 +12,10 @@ class SearchSet(Iterator[Search]):
     including adding, removing, serializing, saving, loading, and clearing the collection.
     It supports iteration, indexing, and conversion to and from JSON-compatible formats.
 
-    Attributes
+    Parameters
     ----------
     searches : list of Search
         The list of Search objects in the collection.
-
-    Methods
-    -------
-    add(search)
-        Add a Search instance to the collection.
-    remove(index)
-        Remove a Search instance from the collection by its index.
-    to_list()
-        Convert all Search objects in the collection to a list of dictionaries.
-    to_json()
-        Convert the entire SearchSet collection to a JSON string.
-    save(path)
-        Save all Search instances in the collection to a JSON file.
-    load(path)
-        Load a SearchSet collection from a JSON file.
 
     Examples
     --------
@@ -48,24 +33,17 @@ class SearchSet(Iterator[Search]):
     """
 
     def __init__(self, searches: list[Search] = None) -> None:
-        """
-        Initializes the object with an optional list of Search instances.
-
-        Parameters
-        ----------
-        searches : list of Search, optional
-            A list of Search objects to initialize the instance with. Defaults to an empty list if not provided.
-
-        Returns
-        -------
-        None
-        """
         self.searches = searches or []
         self._index = 0
 
     def __iter__(self) -> "SearchSet":
         """
         Reset iteration and return self.
+        This method is called when the iterator is initialized, allowing iteration over the SearchSet.
+
+        Returns
+        -------
+        SearchSet
         """
         self._index = 0
         return self
@@ -102,6 +80,29 @@ class SearchSet(Iterator[Search]):
         if 0 <= index < len(self.searches):
             return self.searches[index]
         raise IndexError(f"Index {index} out of range for searches collection of size {len(self.searches)}")
+
+    def __len__(self) -> int:
+        """
+        Get the number of searches in the collection.
+
+        Returns:
+            int: The number of Search instances in the collection.
+        """
+        return len(self.searches)
+
+    def __add__(self, other: "SearchSet") -> "SearchSet":
+        """
+        Combine two SearchSet instances into a new SearchSet.
+
+        Args:
+            other (SearchSet): Another SearchSet instance to combine with.
+
+        Returns:
+            SearchSet: A new SearchSet containing searches from both instances.
+        """
+        if not isinstance(other, SearchSet):
+            raise TypeError("Can only add another SearchSet instance")
+        return SearchSet(self.searches + other.searches)
 
     def __setitem__(self, index: int, value: Search) -> None:
         """
@@ -193,18 +194,28 @@ class SearchSet(Iterator[Search]):
         """
         return [s.to_dict() for s in self.searches]
 
-    def to_json(self) -> str:
+    def to_json(self, path: str = None) -> str:
         """
-        Convert the entire SearchSet collection to a JSON string.
+        Convert the entire SearchSet collection to a JSON string or save to a file.
 
-        Serializes the collection of Search objects into a JSON string format.
-        This is useful for storing or transmitting the search configurations in a
-        standardized format.
+        If a file path is provided, the JSON string is saved to that file.
+        Otherwise, the JSON string is returned.
+
+        Parameters
+        ----------
+        path : str, optional
+            The file path where the JSON data will be written. If not provided,
+            the JSON string is returned.
 
         Returns
         -------
         str
-            A JSON string representation of the SearchSet collection.
+            A JSON string representation of the SearchSet collection if no path is provided.
+
+        Raises
+        ------
+        IOError
+            If the file cannot be written.
 
         Examples
         --------
@@ -214,44 +225,18 @@ class SearchSet(Iterator[Search]):
         >>> json_str = searches.to_json()
         >>> isinstance(json_str, str)
         True
-        """
-        return json_dumps(self.to_list())
-
-    def save(self, path: str) -> None:
-        """
-        Save all Search instances in the collection to a JSON file.
-
-        Serializes the entire collection of Search objects to a JSON file using
-        the `json_dumps` utility. This allows for persistent storage and later
-        retrieval of multiple search configurations.
-
-        Parameters
-        ----------
-        path : str
-            The file path where the JSON data will be written.
-
-        Raises
-        ------
-        IOError
-            If the file cannot be written.
-
-        Examples
-        --------
-        Save a collection of searches to a file:
-
-        >>> s1 = Search(question="Python basics", n_results=2)
-        >>> s2 = Search(question="PEP8 guidelines", n_results=1)
-        >>> searches = SearchSet([s1, s2])
-        >>> searches.save("searches.json")
-
+        >>> searches.to_json("searches.json")
         # The file 'searches.json' will contain both search queries in JSON format.
         """
         data = json_dumps(self.to_list())
-        with open(path, "w") as f:
-            f.write(data)
+        if path:
+            with open(path, "w") as f:
+                f.write(data)
+            return None
+        return data
 
     @classmethod
-    def load(cls, path: str) -> "SearchSet":
+    def from_json(cls, path: str) -> "SearchSet":
         """
         Load a SearchSet collection from a JSON file.
 
@@ -285,7 +270,7 @@ class SearchSet(Iterator[Search]):
         >>> s2 = Search(question="PEP8 guidelines", n_results=1)
         >>> searches = SearchSet([s1, s2])
         >>> searches.save("searches.json")
-        >>> loaded_searches = SearchSet.load("searches.json")
+        >>> loaded_searches = SearchSet.from_json("searches.json")
         >>> print([s.question for s in loaded_searches])
         ['Python basics', 'PEP8 guidelines']
         """
