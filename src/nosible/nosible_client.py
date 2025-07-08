c0769c804ae033bb9aa1a311b4d3fda99697676e
Richard Taylor <richard@nosible.com>
2025-07-08 11:39:21 +0200

    [0.1.9] Bugifx: Bulk search will no longer overload the server

--- src/nosible/nosible_client.py
+++ src/nosible/nosible_client.py
@@ -786,14 +786,26 @@ class Nosible:
         --------
         >>> from nosible.classes.search import Search
         >>> from nosible import Nosible
+        >>> import random
+        >>> questions = [
+        ...     "Why are Nvidia insiders selling over $1 billion in stock?",
+        ...     "What prompted Nvidia executives to dump more than $1B in shares?",
+        ...     "Why did Nvidia insiders divest over $1 billion worth of stock?",
+        ...     "What’s behind Nvidia insiders offloading $1B+ in company shares?",
+        ...     "How did Nvidia execs end up selling more than $1 billion in Nvidia stock?",
+        ...     "What caused Nvidia insiders to unload upwards of $1B in shares?",
+        ...     "Why are Nvidia executives dumping over $1,000,000,000 in stock?",
+        ...     "What’s the story behind Nvidia insiders selling $1B worth of shares?",
+        ...     "What reasons did Nvidia insiders give for selling over $1 billion in stock?",
+        ...     "How did Nvidia insiders manage to sell more than $1B in company stock?" ]
         >>> with Nosible(exclude_netlocs=["bbc.com"]) as nos:
-        ...     results = nos.bulk_search(question="Nvidia insiders dump more than $1 billion in stock", n_results=2000)
+        ...     results = nos.bulk_search(question=random.choice(questions), n_results=2000)
         ...     print(isinstance(results, ResultSet))
         ...     print(len(results))
         True
         2000
 
-        >>> s = Search(question="OpenAI", n_results=1000)
+        >>> s = Search(question=random.choice(questions), n_results=1000)
         >>> with Nosible() as nos:
         ...     results = nos.bulk_search(search=s)
         ...     print(isinstance(results, ResultSet))
@@ -808,17 +820,17 @@ class Nosible:
         TypeError: Either question or search must be specified
 
         >>> nos = Nosible(nosible_api_key="test|xyz")
-        >>> nos.bulk_search(question="foo", search=Search(question="foo")) # doctest: +ELLIPSIS
+        >>> nos.bulk_search(question=random.choice(questions), search=Search(question="foo")) # doctest: +ELLIPSIS
         Traceback (most recent call last):
         ...
         TypeError: Question and search cannot be both specified
         >>> nos = Nosible(nosible_api_key="test|xyz")
-        >>> nos.bulk_search(question="foo", n_results=100) # doctest: +ELLIPSIS
+        >>> nos.bulk_search(question=random.choice(questions), n_results=100) # doctest: +ELLIPSIS
         Traceback (most recent call last):
         ...
         ValueError: Bulk search must have at least 1000 results per query; use search() for smaller result sets.
         >>> nos = Nosible(nosible_api_key="test|xyz")
-        >>> nos.bulk_search(question="foo", n_results=10001)  # doctest: +ELLIPSIS
+        >>> nos.bulk_search(question=random.choice(questions), n_results=10001)  # doctest: +ELLIPSIS
         Traceback (most recent call last):
         ...
         ValueError: Bulk search cannot have more than 10000 results per query.
